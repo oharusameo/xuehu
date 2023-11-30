47fc086bf6eabbedd282da58e8afcccafd213be1
harusame <ggzst321@outlook.com>
2023-11-30 08:59:40 +0800

    完成adminUser网关开发

---
+++ xuehu-gateway/admin-service-gateway/src/main/java/com/harusame/template/config/SaTokenConfig.java
@@ -0,0 +1,45 @@
+package com.harusame.template.config;
+
+import cn.dev33.satoken.jwt.StpLogicJwtForStateless;
+import cn.dev33.satoken.reactor.filter.SaReactorFilter;
+import cn.dev33.satoken.router.SaRouter;
+import cn.dev33.satoken.stp.StpLogic;
+import cn.dev33.satoken.stp.StpUtil;
+import cn.dev33.satoken.util.SaResult;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class SaTokenConfig {
+    @Bean
+    public StpLogic getStpLogicJwt() {
+        return new StpLogicJwtForStateless();
+    }
+
+
+    // 注册 Sa-Token全局过滤器
+    @Bean
+    public SaReactorFilter getSaReactorFilter() {
+        return new SaReactorFilter()
+                // 拦截地址
+                .addInclude("/**")    /* 拦截全部path */
+                // 开放地址
+                .addExclude("/favicon.ico","/api/v1/adminUser/login")
+                // 鉴权方法：每次访问进入
+                .setAuth(obj -> {
+                    // 登录校验 -- 拦截所有路由，并排除/user/doLogin 用于开放登录
+                    SaRouter.match("/**", "/api/v1/adminUser/login", r -> StpUtil.checkLogin());
+
+                    // 权限认证 -- 不同模块, 校验不同权限
+/*                    SaRouter.match("/user/**", r -> StpUtil.checkPermission("user"));
+                    SaRouter.match("/admin/**", r -> StpUtil.checkPermission("admin"));
+                    SaRouter.match("/goods/**", r -> StpUtil.checkPermission("goods"));
+                    SaRouter.match("/orders/**", r -> StpUtil.checkPermission("orders"));*/
+
+                    // 更多匹配 ...  */
+                })
+                // 异常处理方法：每次setAuth函数出现异常时进入
+                .setError(e -> SaResult.error(e.getMessage()));
+    }
+}
