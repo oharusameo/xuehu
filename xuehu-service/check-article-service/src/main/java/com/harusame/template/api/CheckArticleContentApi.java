69fa870306f781ad180e2a090cf3742015792609
harusame <ggzst321@outlook.com>
2023-12-11 19:40:57 +0800

    完成预发布文章接口，完成图像、文本内容识别

--- xuehu-service/check-article-service/src/main/java/com/harusame/template/api/CheckArticleContentApi.java
+++ xuehu-service/check-article-service/src/main/java/com/harusame/template/api/CheckArticleContentApi.java
@@ -21,8 +21,6 @@ import javax.validation.constraints.NotBlank;
 public class CheckArticleContentApi {
     @Resource
     private CheckContentService checkContentService;
-//    html = "<html><head>图片</head><body><img src="https://uploads.kcwiki.cn/commons/0/0a/400px-%E7%99%BE%E7%A7%91%E5%A8%98%E7%BD%91%E7%AB%991-tiny.png"><span>这是一段话</span></body></html>";
-
     @PostMapping("/checkArticleContent")
     @ApiOperation("检查文章内容接口")
     public Result checkArticleContent(@RequestBody @NotBlank @Valid @ApiParam("html") String html) {
